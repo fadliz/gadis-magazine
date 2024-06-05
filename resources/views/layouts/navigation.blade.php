7ceda670a98ffb9e3354d053371f1754e6d7b321
Fadli Zuhri <71310864+fadliz@users.noreply.github.com>
2024-06-05 21:37:27 +0700

    fix profile navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,15 +7,10 @@
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <img src="{{ asset('img/logo.png') }}" alt="Logo"
-                            class="block h-6 w-auto fill-current text-gray-800" />
+                            class="block h-7 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
                 <!-- Navigation Links -->
-                {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div> --}}
             </div>
 
             <!-- Settings Dropdown -->
@@ -34,13 +29,65 @@
                             class="block w-full pl-10 pr-44 py-1 border border-gray-300 rounded-full shadow-sm placeholder-gray-300 focus:outline-none focus:ring focus:border-blue-300 sm:text-sm" />
                     </div>
                 </div>
-                <div class="sm:ml-2"></div>
+
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
-                    <a href="{{ route('profile.edit') }}"
-                        class="text-sm text-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 font-inter">{{ Auth::user()->name }}</a>
-                    <div class="sm:ml-4"></div>
-                    <img src="{{ asset('img/fem-profile-placeholder.png') }}" alt="Profile Picture"
-                        class="h-8 w-8 rounded-full">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-inter font-medium rounded-md text-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                <div>{{ Auth::user()->name }}</div>
+
+                                <div class="ms-1">
+                                    {{-- <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 111.414 1.414l-4 4a1 1 01-1.414 0l-4-4a1 1 010-1.414z" clip-rule="evenodd" />
+                                    </svg> --}}
+                                </div>
+                                <div class="sm:ml-2"></div>
+                                <img src="{{ asset('img/fem-profile-placeholder.png') }}" alt="Profile Picture"
+                                    class="h-8 w-8 rounded-full">
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('profile.edit')" class="flex align-center font-inter ml-1">
+                                <svg width="15" height="19" viewBox="0 0 17 21" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg" class="mr-[10px]">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M8.17048 13.5317C4.30286 13.5317 1 14.1165 1 16.4584C1 18.8003 4.2819 19.406 8.17048 19.406C12.0381 19.406 15.34 18.8203 15.34 16.4793C15.34 14.1384 12.059 13.5317 8.17048 13.5317Z"
+                                        stroke="#6A6A6A" stroke-width="1.5" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M8.17046 10.1914C10.7086 10.1914 12.7657 8.13333 12.7657 5.59524C12.7657 3.05714 10.7086 1 8.17046 1C5.63237 1 3.57425 3.05714 3.57425 5.59524C3.5657 8.12476 5.60951 10.1829 8.13808 10.1914H8.17046Z"
+                                        stroke="#6A6A6A" stroke-width="1.42857" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                </svg>
+                                {{ __('Profile') }}
+                            </x-dropdown-link>
+
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                    this.closest('form').submit();" class="flex align-center text-red-600 font-inter ml-1">
+                                    <svg width="18" height="18" viewBox="0 0 21 21" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg" class="mr-[7px]">
+                                        <path
+                                            d="M13.1393 6.46581V5.64944C13.1393 3.86881 11.6955 2.42506 9.91491 2.42506H5.64928C3.86953 2.42506 2.42578 3.86881 2.42578 5.64944V15.3882C2.42578 17.1688 3.86953 18.6126 5.64928 18.6126H9.92366C11.699 18.6126 13.1393 17.1732 13.1393 15.3978V14.5727"
+                                            stroke="#FF1818" stroke-width="1.5" stroke-linecap="round"
+                                            stroke-linejoin="round" />
+                                        <path d="M19.0837 10.5187H8.54785" stroke="#FF1818" stroke-width="1.5"
+                                            stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M16.5215 7.96802L19.0835 10.5186L16.5215 13.0701" stroke="#FF1818"
+                                            stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+
+                    </x-dropdown>
                 </div>
             </div>
 
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,45 +5,11 @@
         </h2>
     </x-slot> --}}
 
-    <div class="flex">
-        <!-- Sidebar -->
-        <div class="w-1/6 p-4 bg-white">
-            <ul class="mt-6">
-                <li class="flex items-center ml-6">
-                    <svg width="17" height="21" viewBox="0 0 17 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M8.17048 13.5317C4.30286 13.5317 1 14.1165 1 16.4584C1 18.8003 4.2819 19.406 8.17048 19.406C12.0381 19.406 15.34 18.8203 15.34 16.4793C15.34 14.1384 12.059 13.5317 8.17048 13.5317Z"
-                            stroke="#6A6A6A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M8.17046 10.1914C10.7086 10.1914 12.7657 8.13333 12.7657 5.59524C12.7657 3.05714 10.7086 1 8.17046 1C5.63237 1 3.57425 3.05714 3.57425 5.59524C3.5657 8.12476 5.60951 10.1829 8.13808 10.1914H8.17046Z"
-                            stroke="#6A6A6A" stroke-width="1.42857" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <a href="{{ route('profile.edit') }}" class="text-gray-600 font-inter ml-4">Edit Profile</a>
-                </li>
-                <li class="flex items-center ml-6 mt-6">
-                    <svg width="21" height="21" viewBox="0 0 21 21" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M13.1393 6.46581V5.64944C13.1393 3.86881 11.6955 2.42506 9.91491 2.42506H5.64928C3.86953 2.42506 2.42578 3.86881 2.42578 5.64944V15.3882C2.42578 17.1688 3.86953 18.6126 5.64928 18.6126H9.92366C11.699 18.6126 13.1393 17.1732 13.1393 15.3978V14.5727"
-                            stroke="#FF1818" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M19.0837 10.5187H8.54785" stroke="#FF1818" stroke-width="1.5" stroke-linecap="round"
-                            stroke-linejoin="round" />
-                        <path d="M16.5215 7.96802L19.0835 10.5186L16.5215 13.0701" stroke="#FF1818" stroke-width="1.5"
-                            stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <a href="{{ route('logout') }}" class="text-red-600 font-inter ml-3"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                        @csrf
-                    </form>
-                </li>
-            </ul>
-        </div>
-
+    <div class="flex items-center justify-center">
         <!-- Main Content -->
-        <div class="w-3/4 p-4">
+        <div class="w-3/4 p-4 mx-auto">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div class="flex items-center w-full p-6 ">
+                <div class="flex items-center w-full p-6">
                     <button
                         class="text-pink-500 flex items-center border border-pink-500 rounded-full px-5 py-3 hover:bg-pink-100"
                         style="background-color: #FFEAEA;">
@@ -54,23 +20,36 @@
                         </svg>
                         <span>Back</span>
                     </button>
-                    <h1 class="tracking-wide subpixel-antialiased text-3xl font-extrabold font-rubik text-center mx-auto">Edit Profile</h1>
+                    <h1
+                        class="tracking-wide subpixel-antialiased text-3xl font-extrabold font-rubik text-center mx-auto">
+                        Edit Profile</h1>
                 </div>
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        @include('profile.partials.update-profile-information-form')
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex">
+                    <div class="w-1/4 flex flex-col items-center">
+                        <img src="path/to/profile-image.png" alt="Profile Image" class="w-32 h-32 rounded-full mb-4">
+                        <label class="text-center">Photo Profile</label>
+                        <button class="mt-4 py-2 px-4 border rounded bg-gray-200">Choose File</button>
                     </div>
-                </div>
-
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        @include('profile.partials.update-password-form')
-                    </div>
-                </div>
-
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="max-w-xl">
-                        @include('profile.partials.delete-user-form')
+                    <div class="w-3/4 ml-8">
+                        <div class="grid grid-cols-2 gap-4">
+                            <div>
+                                <label for="username">Username</label>
+                                <input type="text" id="username" class="border rounded w-full py-2 px-3">
+                            </div>
+                            <div>
+                                <label for="name">Name</label>
+                                <input type="text" id="name" class="border rounded w-full py-2 px-3">
+                            </div>
+                            <div>
+                                <label for="email">Email</label>
+                                <input type="email" id="email" class="border rounded w-full py-2 px-3">
+                            </div>
+                            <div>
+                                <label for="password">Password</label>
+                                <input type="password" id="password" class="border rounded w-full py-2 px-3">
+                            </div>
+                        </div>
+                        <button class="mt-8 py-2 px-4 bg-pink-500 text-white rounded">Save</button>
                     </div>
                 </div>
             </div>
